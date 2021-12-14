5df2ece0ffa2d34c40dd91a2039e52e58713adcc
Matusek Márton <matusekma@gmail.com>
2021-12-14 11:09:24 +0100

    Implement part1

---
+++ src/main/kotlin/day1/part1.kt
@@ -0,0 +1,19 @@
+package day1
+
+import java.io.File
+
+fun didIncrease(depth1: Int, depth2: Int) = depth1 < depth2
+
+fun countIncreases(): Int {
+    var prevDepth = Int.MAX_VALUE
+    var count = 0
+    val path = {}.javaClass.classLoader.getResource("input.txt")?.file!!
+    File(path).forEachLine { line ->
+        val currentDepth = line.toInt()
+        if (didIncrease(prevDepth, currentDepth)) {
+            count++
+        }
+        prevDepth = currentDepth
+    }
+    return count
+}
