b8075ae0c4e7e09d51c16fb6eaa7055154a49462
Matusek Márton <matusekma@gmail.com>
2021-12-14 11:25:29 +0100

    Make part2 more effective

--- src/main/kotlin/day1/part2.kt
+++ src/main/kotlin/day1/part2.kt
@@ -4,21 +4,22 @@ import java.io.File
 import java.util.*
 
 fun countWindowIncreases(): Int {
-    val window = LinkedList<Int>()
+    val window = LinkedList(listOf(Int.MAX_VALUE / 3, Int.MAX_VALUE / 3, Int.MAX_VALUE / 3))
     var count = 0
-    var lineProcessed = 1;
+    var lineProcessed = 1
     val path = {}.javaClass.classLoader.getResource("input.txt")?.file!!
     File(path).forEachLine { line ->
         val currentDepth = line.toInt()
         window.add(currentDepth)
-        if (lineProcessed > 3) {
-            val prevWindow = window.subList(0, 3).reduce { acc, depth -> acc + depth }
-            val currentWindow = window.subList(1, 4).reduce { acc, depth -> acc + depth }
-            if (didIncrease(prevWindow, currentWindow)) {
-                count++
-            }
-            window.removeFirst()
+
+        val prevWindow = window.subList(0, 3).reduce { acc, depth -> acc + depth }
+        val currentWindow = window.subList(1, 4).reduce { acc, depth -> acc + depth }
+
+        if (didIncrease(prevWindow, currentWindow)) {
+            count++
         }
+
+        window.removeFirst()
         lineProcessed++
     }
     return count
